cb38d7eb4488fbb75681b38b8134cdd84948707c
Mayanksandikar <mayanksandikar191098@gmail.com>
2024-03-21 22:24:35 +0530

    n

--- Index.js
+++ Index.js
@@ -1,53 +1,13 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
-dotenv.config();
-const cors = require("cors");
+import express from "express";
 
 const app = express();
 
-// CORS configuration to allow requests from a specific origin
-app.use(cors({
-  origin: ["https://student-data-9os7.vercel.app"],
-  methods: ["POST", "GET"],
-  credentials:true
-}));
+const port = 9000;
 
-// Middleware to parse JSON request bodies
-app.use(express.json());
-
-// Connect to the MongoDB database
-const URI = "mongodb+srv://mayanksandikar191098:P612SpRzc2bEeue0@reactjs.smvpawt.mongodb.net/SchoolTeacher?retryWrites=true&w=majority&appName=reactjs";
- 
-mongoose.connect(URI, {
-}).then(() => {
-  console.log("Connected to the database successfully");
-}).catch((error) => {
-  console.log("Error connecting to the database:", error);
-});
-
-
-app.get("/", (req, res) => {
-  
+app.use("/", (req, res) => {
   res.json({ message: "Hello Vercel" });
 });
 
-// Route for handling student-related routes
-const userStudentRoute = require("./routes/userStudentRoutes");
-app.use("/students", userStudentRoute);
-
-app.get("/students" , (req, res)=>{
-  res.json("hello")
-})
-// Route for handling teacher-related routes
-const userTeacherRoute = require("./routes/userRoutes");
-app.use("/teachers", userTeacherRoute);
-
-// Middleware to ignore favicon.ico requests
-app.use('/favicon.ico', (req, res) => res.status(204));
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(9000, () => {
+  console.log(`Server is running on Port ${port}`);
 });
