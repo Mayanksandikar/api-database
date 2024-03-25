fde2e623b6687e309394559eb91deb8f6829c51b
Mayanksandikar <mayanksandikar191098@gmail.com>
2024-03-25 13:52:53 +0530

    n

--- Index.js
+++ Index.js
@@ -1,29 +1,24 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const cors = require("cors");
-
-const userStudentRoute = require("./routes/userStudentRoutes");
-const userTeacherRoute = require("./routes/userRoutes");
-
 dotenv.config();
+const cors = require("cors");
 
 const app = express();
 
 // CORS configuration to allow requests from a specific origin
-// app.use(cors({
-//   origin: ["https://student-data-9os7.vercel.app"],
-//   methods: ["POST", "GET"],
-//   credentials:true
-// }));
+app.use(cors({
+  origin: ["https://student-data-9os7.vercel.app"],
+  methods: ["POST", "GET"],
+  credentials:true
+}));
 
 // Middleware to parse JSON request bodies
 app.use(express.json());
 
 // Connect to the MongoDB database
-const URI = "mongodb+srv://mayanksandikar191098:P612SpRzc2bEeue0@reactjs.smvpawt.mongodb.net/krt?retryWrites=true&w=majority&appName=reactjs"
+const URI = "mongodb+srv://mayanksandikar191098:P612SpRzc2bEeue0@reactjs.smvpawt.mongodb.net/krt?retryWrites=true&w=majority&appName=reactjs";
  
-
 mongoose.connect(URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
@@ -33,21 +28,25 @@ mongoose.connect(URI, {
   console.log("Error connecting to the database:", error);
 });
 
-// Route for handling student-related routes
-app.use("/students", userStudentRoute);
-
-// Route for handling teacher-related routes
-app.use("/teachers", userTeacherRoute);
-
-// Middleware to ignore favicon.ico requests
-app.use('/favicon.ico', (req, res) => res.status(204));
-
-// Default route handler
 
 app.get("/", (req, res) => {
   
   res.json({ message: "Hello Vercel" });
 });
+
+// Route for handling student-related routes
+const userStudentRoute = require("./routes/userStudentRoutes");
+app.use("/students", userStudentRoute);
+
+app.get("/students" , (req, res)=>{
+  res.json("hello")
+})
+// Route for handling teacher-related routes
+const userTeacherRoute = require("./routes/userRoutes");
+app.use("/teachers", userTeacherRoute);
+
+// Middleware to ignore favicon.ico requests
+app.use('/favicon.ico', (req, res) => res.status(204));
 
 const PORT = process.env.PORT || 5000;
 
