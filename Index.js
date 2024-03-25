c86f7c16f938001b1d5327129cd8a006e8cf2dbf
Mayanksandikar <mayanksandikar191098@gmail.com>
2024-03-25 14:22:50 +0530

    n

--- Index.js
+++ Index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-dotenv.config();
 const cors = require("cors");
+const userStudentRoute = require("./routes/userStudentRoutes");
+const userTeacherRoute = require("./routes/userRoutes");
+
+dotenv.config();
 
 const app = express();
 
@@ -10,37 +13,34 @@ const app = express();
 app.use(cors({
   origin: ["https://student-data-9os7.vercel.app"],
   methods: ["POST", "GET"],
-  credentials:true
+  credentials: true
 }));
 
 // Middleware to parse JSON request bodies
 app.use(express.json());
 
 // Connect to the MongoDB database
+
 const URI = "mongodb+srv://mayanksandikar191098:P612SpRzc2bEeue0@reactjs.smvpawt.mongodb.net/krt?retryWrites=true&w=majority&appName=reactjs";
- 
+
+
 mongoose.connect(URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 }).then(() => {
   console.log("Connected to the database successfully");
 }).catch((error) => {
   console.log("Error connecting to the database:", error);
 });
 
-
 app.get("/", (req, res) => {
-  
   res.json({ message: "Hello Vercel" });
 });
 
 // Route for handling student-related routes
-const userStudentRoute = require("./routes/userStudentRoutes");
 app.use("/students", userStudentRoute);
 
-
 // Route for handling teacher-related routes
-const userTeacherRoute = require("./routes/userRoutes");
 app.use("/teachers", userTeacherRoute);
 
 // Middleware to ignore favicon.ico requests
