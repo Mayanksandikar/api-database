51afa1a7186c21cea698dc8f3f74d84f18e9a881
Mayanksandikar <mayanksandikar191098@gmail.com>
2024-03-25 15:54:34 +0530

    n

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/userTeacher"); // Adjust the path to userModel based on your project structure
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcryptjs");
 // Create and send data to the database
 router.post('/signup', async (req, res) => {
     const { name, email, password } = req.body;
